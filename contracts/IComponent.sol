e88b12695c6c709c2aa8a523d345566c249728f4
Cosmin Nechifor <cosmin.nechifor96@gmail.com>
2019-03-06 18:11:30 +0200

    changed factory contract and interface

--- contracts/IComponent.sol
+++ contracts/IComponent.sol
@@ -1,13 +1,13 @@
 pragma solidity >=0.4 <0.6.0; 
 
-interface IComponent {
+interface IComponent{
     function updateParentAddress(address _parentComponentAddress) external;
     function updateData(string calldata _data) external; 
-    function addChild(address _childComponentAddress) external;
-    function removeChild(address _childComponentAddress) external;
+    function addChild(address _childComponentAddress) external; 
+    function removeChild(uint256 _index) external;
     function getData() external view returns (string memory);
-    function getOwner() external view returns (address);
-    function getChildComponentNumber() external view returns(uint256);
-    function getChildComponentIndexByAddress(address _childComponentAddress) external view returns(uint256);
+    function getParentComponentAddress() external view returns (address);
+    function getNumberOfChildComponents() external view returns(uint256);
+    function getChildComponentList() external view returns(address[] memory);
     function getChildComponentAddressById(uint256 _childComponentAddress) external view returns(address);
 }
