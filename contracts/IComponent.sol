fe29deaa619bab8ba4c07300d998d9b2ef44bbc7
Cosmin Nechifor <cosmin.nechifor96@gmail.com>
2019-03-06 19:34:08 +0200

    added tests for ComponentContract

--- contracts/IComponent.sol
+++ contracts/IComponent.sol
@@ -4,7 +4,7 @@ interface IComponent{
     function updateParentAddress(address _parentComponentAddress) external;
     function updateData(string calldata _data) external; 
     function addChild(address _childComponentAddress) external; 
-    function removeChild(uint256 _index) external;
+    function removeChild(uint256 _index) external returns(address);
     function getData() external view returns (string memory);
     function getParentComponentAddress() external view returns (address);
     function getNumberOfChildComponents() external view returns(uint256);
