52586ef1285b4acf936e48100fd34b62c0bc28eb
Cosmin Nechifor <cosmin.nechifor96@gmail.com>
2019-03-08 00:04:05 +0200

    changed registry

--- contracts/IRegistry.sol
+++ contracts/IRegistry.sol
@@ -6,4 +6,6 @@ interface IRegistry {
     function getRegistrySize() external view returns(uint256);
     function getRegistredComponentAtIndex(uint256 _index) external view returns(address);
     function getRegistredComponents() external view returns(address[] memory);
+    function owner() external view returns (address);
+    function transferOwnership(address newOwner) external;
 }
