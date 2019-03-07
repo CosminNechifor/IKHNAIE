121e1ee150b72e88afb0789c0b30e6406ba90858
Cosmin Nechifor <cosmin.nechifor96@gmail.com>
2019-03-07 23:09:53 +0200

    added registry

---
+++ contracts/Registry.sol
@@ -0,0 +1,52 @@
+pragma solidity >=0.4 <0.6.0; 
+
+import "./Ownable.sol";
+import "./IComponent.sol";
+import "./IComponentFactory.sol";
+
+
+contract Registry is Ownable {
+
+    // the place where all components in the system are going to be stored
+    address[] private _registry;
+
+
+    function addComponent(address _componentAddress) external {
+        _registry.push(_componentAddress);
+    }
+
+    // TODO: migh need to be changed/removed 
+    function removeComponent(uint256 _index) external {
+        uint256 lastElementIndex = _registry.length - 1;
+        address _componentAddress = _registry[lastElementIndex]; 
+
+        _registry[_index] = _componentAddress;
+        
+        // deleteing the element and enusre there is no empty space
+        delete _registry[lastElementIndex];
+        _registry.length--;
+    }
+
+    function getRegistrySize() external view returns(uint256) {
+        return _registry.length;
+    }
+
+    function getRegistredComponentAtIndex(
+        uint256 _index
+    )
+        external
+        view
+        returns(address)
+    {
+        return _registry[_index];
+    }
+
+    function getRegistredComponents() 
+        external 
+        view 
+        returns(address[] memory)
+    {
+        return _registry;
+    }
+
+}
