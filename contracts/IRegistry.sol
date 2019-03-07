7ca36c0da0c17386efdca087b4b577490296938e
Cosmin Nechifor <cosmin.nechifor96@gmail.com>
2019-03-07 23:16:22 +0200

    implemented the IRegistry interface

---
+++ contracts/IRegistry.sol
@@ -0,0 +1,9 @@
+pragma solidity >=0.4 <0.6.0; 
+
+interface IRegistry {
+    function addComponent(address _componentAddress) external;
+    function removeComponent(uint256 _index) external;
+    function getRegistrySize() external view returns(uint256);
+    function getRegistredComponentAtIndex(uint256 _index) external view returns(address);
+    function getRegistredComponents() external view returns(address[] memory);
+}
