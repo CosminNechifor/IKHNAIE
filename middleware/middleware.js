9459bcb4d21e0a312220edfa0a4387604ecbeb18
Cosmin Nechifor <cosmin.nechifor96@gmail.com>
2019-03-17 20:23:55 +0200

    integration with web3

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,11 +1,25 @@
 import express from 'express';
+import Web3 from 'web3';
 
 const app = express()
 app.use(express.json())
 
+const web3 = new Web3(Web3.givenProvider || "ws://localhost:8540");
+console.log(web3);
+
+// test connections
+const getBlockNumber = () => {
+    web3.eth.getBlock(1, (error, result) => {
+        if(!error)
+            console.log(JSON.stringify(result));
+        else
+            console.error(error);
+    });
+}
 
 app.get('/', (req, res) => {
-    return res.status(200).send({'message': 'Middleware is running'});
+    getBlockNumber()
+    return res.status(200).send({'message': 'worked' });
 })
 
 
