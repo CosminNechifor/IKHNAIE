82874d9d562c8f26e2e24826a69397fc6d231cb0
Cosmin Nechifor <cosmin.nechifor96@gmail.com>
2019-03-18 10:50:10 +0200

    access implemented

--- middleware/access.js
+++ middleware/access.js
@@ -1,7 +1,7 @@
 import Web3 from 'web3';
 import * as fs from 'fs';
 
-const contractAddress = '0xE29142470E9d86441EeC6b8B1014650Eb0c47104';
+const contractAddress = '0xad934659b1e341dE46442E77437655B51aDe3dB3';
 const priv_key = '0x876258c7c6c0eecff9c179a76b2f002bc9a35cde23ff4fbc7df17f8f683e1614'
 
 const web3 = new Web3(Web3.givenProvider || "ws://localhost:8540");
@@ -24,13 +24,79 @@ const account = web3.eth.accounts.privateKeyToAccount(priv_key);
 
 
 const getRegistrySize = () => {
-    return contract.methods.getRegistrySize.call({from: account.address});
+    return contract.methods
+        .getRegistrySize
+        .call({from: account.address});
 }
 
 const createComponent = (data) => {
-    return contract.methods.createComponent(data).send({from: account.address, gas: 3900000});
+    return contract.methods
+        .createComponent(data)
+        .send({from: account.address, gas: 3900000});
 } 
 
-export {getRegistrySize, createComponent};
+const getRegistredComponents = () => {
+    return contract.methods
+        .getRegistredComponents()
+        .call({from: account.address});
+}
+
+const getRegistredComponentAtIndex = (index) => {
+    return contract.methods
+        .getRegistredComponentAtIndex(index)
+        .call({from: account.address});
+} 
+
+const getComponentOwner = (address) => {
+    return contract.methods
+        .getComponentOwner(address)
+        .call({from: account.address});
+} 
+
+const getComponentData = (address) => {
+    return contract.methods
+        .getComponentData(address)
+        .call({from: account.address});
+} 
+
+const getChildComponentIndexByAddress = (parentAddress, childAddress) => {
+    return contract.methods
+        .getChildComponentIndexByAddress(parentAddress, childAddress)
+        .call({from: account.address});
+} 
+
+const getChildComponentAddressByIndex = (parentAddress, id) => {
+    return contract.methods
+        .getChildComponentAddressByIndex(parentAddress, id)
+        .call({from: account.address});
+} 
+
+// returns a promise
+const updateData = (address, data) => {
+    return contract.methods
+        .updateData(address, data)
+        .send({from: account.address, gas: 4000000});
+} 
+
+//returns a promise
+const removeChildComponentFromComponent = (parentAddress, id) => {
+    return contract.methods
+        .removeChildComponentFromComponent(parentAddress, id)
+        .send({from: account.address, gas: 4000000});
+} 
+
+//returns a promise
+const addChildComponentToComponent = (parentAddress, childAddress) => {
+    returns contract.methods
+        .addChildComponentToComponent(parentAddress, childAddress)
+        .send({from: account.address, gas: 4000000});
+}
+
+export {
+    getRegistrySize,
+    createComponent,
+    getRegistredComponents,
+    getRegistredComponentAtIndex
+};
 
 
--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,20 +1,43 @@
 import express from 'express';
-import {getRegistrySize, createComponent} from './access.js';
+import {
+    createComponent,
+    getRegistredComponents,
+    getRegistrySize,
+    getRegistredComponentAtIndex
+} from './access.js';
 
 const app = express()
 app.use(express.json())
 
-app.get('/size', (req, res) => {
+app.get('/api/v1/size', (req, res) => {
     getRegistrySize().then(size => {
         res.status(200).send({'size': size });
     });
 });
 
-app.post('/component', (req, res) => {
-    createComponent(req.body.data).then(blockdata=> {
+app.post('/api/v1/component', (req, res) => {
+    createComponent(req.body.data).then(blockdata => {
         res.status(200).send({'blockdata': blockdata}); 
     });
 });
+
+app.get('/api/v1/component', (req, res) => {
+    getRegistredComponents().then(components => {
+        res.status(200).send({'components': components});
+    });
+});
+
+app.get('/api/v1/component', (req, res) => {
+    if (req.query.id) {
+        getRegistredComponentAtIndex(req.query.id)
+            .then(component => {
+                res.status(200).send({'component': component});
+        });
+    } else if (req.query.address) {
+        console.log(req.query.address)
+    }
+});
+
 
 app.listen(3000)
 console.log('app running on port ', 3000);
