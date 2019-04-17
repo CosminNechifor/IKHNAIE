24eb6609c1741a0d516ee4b41ff5260dad4aaee4
Cosmin Nechifor <cosmin.nechifor96@gmail.com>
2019-04-17 23:33:27 +0300

    stash me

--- blockchain/contracts/Storage.sol
+++ blockchain/contracts/Storage.sol
@@ -15,8 +15,13 @@ contract Storage is Ownable, Management {
         _;
     }
 
+    modifier notRootComponent(address _componentAddress) {
+        require(componentToParent[_componentAddress] != address(0));
+        _;
+    }
+
     // This would be helpful in the determination of the parent component of a component
-    mapping(address => address) componentToParent;
+    mapping(address => address) private componentToParent;
 
     // Helpful when finding what components does this owner own. 
     mapping (address => address[]) private ownerToRootComponents;
@@ -47,27 +52,56 @@ contract Storage is Ownable, Management {
         external 
         onlyManager()
         isRootComponent(_childComponent)
-        isRootComponent(_parentComponent)
     {
         componentToParent[_childComponent] = _parentComponent;
         
         // extract information
-        Info memory info = rootComponentToInfo[_childComponent];
+        Info storage info = rootComponentToInfo[_childComponent];
 
         // perform reconstruction of the data structures
-        uint256 lastIndex = ownerToRootComponents[info.owner].lenth - 1;
+        uint256 lastIndex = ownerToRootComponents[info.owner].length - 1;
         // put the last element where the old root component was
         ownerToRootComponents[info.owner][info.index] = ownerToRootComponents[info.owner][lastIndex];
         // delete the last element
         delete ownerToRootComponents[info.owner][lastIndex];
         // repair the length of the array 
-        ownerToRootComponents[info.owner]--;
+        ownerToRootComponents[info.owner].length--;
 
         // this will reset the struct values
         delete rootComponentToInfo[_childComponent];
     }
 
-    function getComponentOwner(address _component) external view returns (address) {
+    // onlyManager()
+    // notInSubmitedForSaleState()
+    // notInNeedsRecycledState()
+    // notInRecycledOrDestoyedState()
+    function removeChildFromParentComponent(
+        address _childComponent
+    ) 
+        external
+        onlyManager()
+        notRootComponent(_childComponent)
+    {
+        
+        // extracting the owner
+        address _owner = getComponentOwner(_childComponent);
+
+        // the removed component becomes a root component
+        componentToParent[_childComponent] == address(0); 
+        
+        // we add the component to the owner components list
+        uint256 _index = ownerToRootComponents[_owner].push(_childComponent) - 1;
+        
+        // creating the component info
+        Info memory info = Info({
+            owner: _owner,
+            index: _index
+        });
+
+        rootComponentToInfo[_childComponent] = info;
+    }
+
+    function getComponentOwner(address _component) public view returns (address) {
         // if the address is equal to 0x00000
         if (componentToParent[_component] == address(0)) {
             // then it's a root component 
