9ab450dba4152013493964b587ff1c9584a00d2b
Cosmin Nechifor <cosmin.nechifor96@gmail.com>
2019-04-20 20:02:04 +0300

    added the MarketPlace interface

---
+++ blockchain/contracts/IMarketPlace.sol
@@ -0,0 +1,14 @@
+pragma solidity >=0.4 <0.6.0; 
+
+interface IMarketPlace is Management { 
+    function submitForSale(address _owner, address _componentAddress) external returns (bool);
+    function removeFromSale( address _owner, address _componentAddress) external returns (bool);
+    function addOffer( address _sender, address _componentAddress, uint256 _amount) external;
+    function acceptOffer( address _owner, address _componentAddress, uint256 _offerIndex) external returns (address, uint256);
+    function rejectOffer( address _componentAddress, uint256 _offerIndex) external returns (bool);
+    function _performStorageCleanup( address _owner, address _componentAddress) private returns(bool);
+    function getComponentsSubmitedForSale() external view returns (address[] memory);
+    function getOwnerComonentsSubmitedForSale( address _owner) external view returns(address[] memory); 
+    function getComponentOfferByIndex( address _componentAddress, uint256 _index) external view returns (uint256, address);
+    function getComponentOfferSize( address _componentAddress) external view returns(uint256); 
+}
