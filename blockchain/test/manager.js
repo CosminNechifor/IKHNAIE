6cc159f2ebe8309a030a98508d81507fd61a73a2
Cosmin Nechifor <cosmin.nechifor96@gmail.com>
2019-05-26 23:01:07 +0300

    implemented test for recycler confirmation

--- blockchain/test/manager.js
+++ blockchain/test/manager.js
@@ -844,39 +844,30 @@ contract('Manager - testing deployment and creation of components [happy case]',
         assert.equal(a7_balance.toNumber(), 1037, "Balance should be 1037"); 
     });
 
-    // it("Register recycler, and check status", async () => {
-    //     await managerContract.registerRecycler(
-    //         accounts[0]
-    //     );
-    //     let status = await managerContract.getProducerStatus(accounts[0]);
-    //     assert.equal(status[0], false, "Status should be false because we didn't send 5 ETH!!");
-    //     assert.equal(status[1], false, "Status should be false because we didn't confirm the Producer!!");
+    it("Register recycler, and check status", async () => {
+        await managerContract.registerRecycler(
+            "Recycler name",
+            "Information about the recycler",
+            {
+                from: accounts[9],
+                value: 1
+            }
+        );
+        let status = await managerContract.getRecyclerInfo(accounts[9]);
+        assert.equal(status[0], "Recycler name", "Wrong recycler name!"); 
+        assert.equal(status[1], "Information about the recycler", "Wrong recycler information!"); 
+        assert.equal(status[2].toNumber(), 0, "Wrong recycled value!"); 
+        assert.equal(status[3], true, "Recycler should be registred!"); 
+        assert.equal(status[4], false, "Recycler should not be confirmed!"); 
 
-    //     await managerContract.registerProducer(
-    //         accounts[0],
-    //         {
-    //             value: 5
-    //         }
-    //     );
-    //     status = await managerContract.getProducerStatus(accounts[0]);
-    //     assert.equal(status[0], true, "Status should be true because we did send 5 ETH!!");
-    //     assert.equal(status[1], false, "Status should be false because we didn't confirm the Producer!!");
+        await managerContract.confirmRecycler(accounts[9]);
 
-    //     await managerContract.confirmProducer(accounts[0]);
-    //     status = await managerContract.getProducerStatus(accounts[0]);
-    //     assert.equal(status[0], true, "Status should be true because we did send 5 ETH!!");
-    //     assert.equal(status[1], true, "Status should be true because we did confirm the Producer!!");
-    //     await managerContract.registerProducer(
-    //         accounts[7],
-    //         {
-    //             from: accounts[7],
-    //             value: 5
-    //         }
-    //     );
-    //     await managerContract.confirmProducer(accounts[7]);
-    //     status = await managerContract.getProducerStatus(accounts[7]);
-    //     assert.equal(status[0], true, "Status should be true because we did send 5 ETH!!");
-    //     assert.equal(status[1], true, "Status should be true because we did confirm the Producer!!");
+        status = await managerContract.getRecyclerInfo(accounts[9]);
+        assert.equal(status[0], "Recycler name", "Wrong recycler name!"); 
+        assert.equal(status[1], "Information about the recycler", "Wrong recycler information!"); 
+        assert.equal(status[2].toNumber(), 0, "Wrong recycled value!"); 
+        assert.equal(status[3], true, "Recycler should be registred!"); 
+        assert.equal(status[4], true, "Recycler should be confirmed!"); 
 
-    // });
+    });
 });
