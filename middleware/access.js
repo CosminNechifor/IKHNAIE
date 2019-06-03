fb3994b65ef4f83831d97aa038553fd44f73f336
Cosmin Nechifor <cosmin.nechifor96@gmail.com>
2019-06-03 17:31:42 +0300

    modified access layer

--- middleware/access.js
+++ middleware/access.js
@@ -23,57 +23,192 @@ const createContractInterface = (json, contractAddress) => {
 const ManagerContract = createContractInterface(getContractJSON(managerContractPath), managerContractAddress);
 const account = web3.eth.accounts.privateKeyToAccount(privKey);
 
-const getRegistrySize = () => {
+// needs to be tested and we need to add
+// an endpoint for it
+const deposit = (data) => {
     return ManagerContract.methods
-        .getRegistrySize
-        .call({from: account.address});
-}
+        .deposit(data)
+        .send({from: account.address, gas: 3900000, value:data.value});
+} 
 
-const createComponent = (data) => {
+// needs to be tested and we need to add
+// an endpoint for it
+const withdraw = (data) => {
     return ManagerContract.methods
-        .createComponent(data)
+        .withdraw(data)
         .send({from: account.address, gas: 3900000});
 } 
 
-const getRegistredComponents = () => {
-    return ManagerContract.methods
-        .getRegistredComponents()
-        .call({from: account.address});
+// needs to be tested and we need to add
+// an endpoint for it
+const registerActor = (data, type) => {
+    
+    // check type before calling register
+    if (type === "Repairer")
+        return ManagerContract.methods
+            .registerRepairer(data)
+            .send({from: account.address, gas: 3900000});
+    if (type === "Recycler")
+        return ManagerContract.methods
+            .registerRecycler(data)
+            .send({from: account.address, gas: 3900000});
+    if (type === "Producer")
+        return ManagerContract.methods
+            .registerProducer(data)
+            .send({from: account.address, gas: 3900000});
+} 
+
+const confirmActor = (data, type) => {
+    if (type === "Repairer")
+        return ManagerContract.methods
+            .confirmRepairer(data)
+            .send({from: account.address, gas: 3900000});
+    if (type === "Recycler")
+        return ManagerContract.methods
+            .confirmRecycler(data)
+            .send({from: account.address, gas: 3900000});
+    if (type === "Producer")
+        return ManagerContract.methods
+            .confirmProducer(data)
+            .send({from: account.address, gas: 3900000});
 }
 
-const getRegistredComponentAtIndex = (index) => {
+const createComponent = (data, amount) => {
     return ManagerContract.methods
-        .getRegistredComponentAtIndex(index)
-        .call({from: account.address});
+        .createComponent(data)
+        .send({from: account.address, gas: 3900000, value: amount});
 } 
 
-const getComponentOwner = (address) => {
+const addChildComponentToComponent = (parentAddress, childAddress) => {
     return ManagerContract.methods
-        .getComponentOwner(address)
-        .call({from: account.address});
+        .addChildComponentToComponent(parentAddress, childAddress)
+        .send({from: account.address, gas: 4000000});
+}
+
+const removeChildComponentFromComponent = (parentAddress, id) => {
+    return ManagerContract.methods
+        .removeChildComponentFromComponent(parentAddress, id)
+        .send({from: account.address, gas: 4000000});
 } 
 
-const getComponentInfo = (address) => {
+const submitComponentToMarket = (data) => {
     return ManagerContract.methods
-        .getComponentInfo(address)
-        .call({from: account.address});
+        .submitComponentToMarket(data)
+        .send({from: account.address, gas: 4000000});
 } 
 
-const getComponentData = (address) => {
+const removeComponentFromMarket = (data) => {
     return ManagerContract.methods
-        .getComponentData(address)
-        .call({from: account.address});
+        .removeComponentFromMarket(data)
+        .send({from: account.address, gas: 4000000});
 } 
 
-const getChildComponentIndexByAddress = (parentAddress, childAddress) => {
+const addOffer = (data) => {
     return ManagerContract.methods
-        .getChildComponentIndexByAddress(parentAddress, childAddress)
-        .call({from: account.address});
+        .addOffer(data)
+        .send({from: account.address, gas: 4000000});
 } 
+
+const removeOffer = (data) => {
+    return ManagerContract.methods
+        .removeOffer(data)
+        .send({from: account.address, gas: 4000000});
+} 
+
+const acceptOffer = (data) => {
+    return ManagerContract.methods
+        .acceptOffer(data)
+        .send({from: account.address, gas: 4000000});
+} 
+
+const rejectOffer = (data) => {
+    return ManagerContract.methods
+        .rejectOffer(data)
+        .send({from: account.address, gas: 4000000});
+} 
+
+const modifyAllowance = (data) => {
+    return ManagerContract.methods
+        .modifyAllowance(data)
+        .send({from: account.address, gas: 4000000});
+} 
+
+const flagComponentAsExpired = (data) => {
+    return ManagerContract.methods
+        .flagComponentAsExpired(data)
+        .send({from: account.address, gas: 4000000});
+} 
+
+const flagComponentAsBroken = (data) => {
+    return ManagerContract.methods
+        .flagComponentAsBroken(data)
+        .send({from: account.address, gas: 4000000});
+}
+
+const repair = (data) => {
+    return ManagerContract.methods
+        .repair(data)
+        .send({from: account.address, gas: 4000000});
+}
+
+const submitFOrRecycling = (data) => {
+    return ManagerContract.methods
+        .submitForRecycling(data)
+        .send({from: account.address, gas: 4000000});
+}
+
+const recycle = (data) => {
+    return ManagerContract.methods
+        .recycle(data)
+        .send({from: account.address, gas: 4000000});
+}
+
+const destroy = (data) => {
+    return ManagerContract.methods
+        .destroy(data)
+        .send({from: account.address, gas: 4000000});
+}
+
+// TODO: Updates might fail
+const updateComponentName = (data) => {
+    return ManagerContract.methods
+        .updateComponentName(data)
+        .send({from: account.address, gas: 4000000});
+}
+
+const updateComponentExpiration = (data) => {
+    return ManagerContract.methods
+        .updateComponentExpiration(data)
+        .send({from: account.address, gas: 4000000});
+}
+
+const updateComponentPrice = (data) => {
+    return ManagerContract.methods
+        .updateComponentPrice(data)
+        .send({from: account.address, gas: 4000000});
+}
+
+const updateComponentOtherInformation = (data) => {
+    return ManagerContract.methods
+        .updateComponentOtherInformation(data)
+        .send({from: account.address, gas: 4000000});
+}
+
+const transferComponentOwnership = (data) => {
+    return ManagerContract.methods
+        .transferComponentOwnership(data)
+        .send({from: account.address, gas: 4000000});
+}
 
 const getChildComponentAddressByIndex = (parentAddress, id) => {
     return ManagerContract.methods
         .getChildComponentAddressByIndex(parentAddress, id)
+        .call({from: account.address});
+}
+
+const getChildComponentIndexByAddress = (parentAddress, childAddress) => {
+    return ManagerContract.methods
+        .getChildComponentIndexByAddress(parentAddress, childAddress)
         .call({from: account.address});
 } 
 
@@ -83,38 +218,153 @@ const getChildComponentListOfAddress = (parentAddress) => {
         .call({from: account.address});
 } 
 
-const updateData = (address, data) => {
+const getComponentData = (address) => {
     return ManagerContract.methods
-        .updateData(address, data)
-        .send({from: account.address, gas: 4000000});
+        .getComponentData(address)
+        .call({from: account.address});
 } 
 
-const removeChildComponentFromComponent = (parentAddress, id) => {
+const getComponentOwner = (address) => {
     return ManagerContract.methods
-        .removeChildComponentFromComponent(parentAddress, id)
-        .send({from: account.address, gas: 4000000});
+        .getComponentOwner(address)
+        .call({from: account.address});
+} 
+const getRegistrySize = () => {
+    return ManagerContract.methods
+        .getRegistrySize
+        .call({from: account.address});
+}
+
+const getRegistredComponentAtIndex = (index) => {
+    return ManagerContract.methods
+        .getRegistredComponentAtIndex(index)
+        .call({from: account.address});
 } 
 
-const addChildComponentToComponent = (parentAddress, childAddress) => {
+const getRegistredComponents = () => {
     return ManagerContract.methods
-        .addChildComponentToComponent(parentAddress, childAddress)
-        .send({from: account.address, gas: 4000000});
+        .getRegistredComponents()
+        .call({from: account.address});
+}
+
+const getComponentsSubmitedForSale = () => {
+    return ManagerContract.methods
+        .getComponentsSubmitedForSale()
+        .call({from: account.address});
+}
+
+// TODO: correct this
+const getComponentOfferSize = (componentAddress) => {
+    return ManagerContract.methods
+        .getComponentOfferSize(componentAddress)
+        .call({from: account.address});
+}
+
+const getComponentOfferByIndex = (componentAddress, index) => {
+    return ManagerContract.methods
+        .getComponentOfferByIndex(componentAddress, index)
+        .call({from: account.address});
+}
+
+const isProducer = (actorAddress) => {
+    return ManagerContract.methods
+        .isProducer(actorAddress)
+        .call({from: account.address});
+}
+
+const isRecycler = (actorAddress) => {
+    return ManagerContract.methods
+        .isRecycler(actorAddress)
+        .call({from: account.address});
+}
+
+const isRepairer = (actorAddress) => {
+    return ManagerContract.methods
+        .isRepairer(actorAddress)
+        .call({from: account.address});
+}
+
+const balance = () => {
+    return ManagerContract.methods
+        .balance()
+        .call({from: account.address});
+}
+
+const getAllowance = (spender) => {
+    return ManagerContract.methods
+        .getAllowance(spender)
+        .call({from: account.address});
+}
+
+const getComponentReward = (componentAddress) => {
+    return ManagerContract.methods
+        .getComponentReward(componentAddress)
+        .call({from: account.address});
+}
+
+const getProducerInfo = (producerAddress) => {
+    return ManagerContract.methods
+        .getProducerInfo(producerAddress)
+        .call({from: account.address});
+}
+
+const getRecyclerInfo = (recyclerAddress) => {
+    return ManagerContract.methods
+        .getRecyclerInfo(recyclerAddress)
+        .call({from: account.address});
+}
+
+const getRepairerInfo = (repairerAddress) => {
+    return ManagerContract.methods
+        .getRepairerInfo(repairerAddress)
+        .call({from: account.address});
 }
 
 export {
-    getRegistrySize,
+    deposit,
+    registerActor,
+    confirmActor,
     createComponent,
-    getRegistredComponents,
-    getRegistredComponentAtIndex,
-    getComponentOwner,
-    getComponentData,
-    getChildComponentIndexByAddress,
-    getChildComponentAddressByIndex,
-    updateData,
-    removeChildComponentFromComponent, 
     addChildComponentToComponent,
+    removeChildComponentFromComponent,
+    submitComponentToMarket,
+    removeComponentFromMarket,
+    addOffer,
+    removeOffer,
+    acceptOffer,
+    rejectOffer,
+    modifyAllowance,
+    flagComponentAsExpired,
+    flagComponentAsBroken,
+    repair,
+    submitFOrRecycling,
+    recycle,
+    destroy,
+    updateComponentName,
+    updateComponentExpiration,
+    updateComponentPrice,
+    updateComponentOtherInformation,
+    transferComponentOwnership,
+    getChildComponentAddressByIndex,
+    getChildComponentIndexByAddress,
     getChildComponentListOfAddress,
-    getComponentInfo,
+    getComponentData,
+    getComponentOwner,
+    getRegistrySize,
+    getRegistredComponentAtIndex,
+    getRegistredComponents,
+    getComponentsSubmitedForSale,
+    getComponentOfferSize,
+    getComponentOfferByIndex,
+    isProducer,
+    isRecycler,
+    isRepairer,
+    balance,
+    getAllowance,
+    getComponentReward,
+    getProducerInfo,
+    getRecyclerInfo,
+    getRepairerInfo
 };
 
 
