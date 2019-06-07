a57cfe4e1dbb00675b6d8fbcb02428981a79e00e
Cosmin Nechifor <cosmin.nechifor96@gmail.com>
2019-06-07 15:47:29 +0300

    implemented balance and deposti in middleware

--- middleware/middleware.py
+++ middleware/middleware.py
@@ -1,5 +1,4 @@
 from web3 import Web3, HTTPProvider
-from web3.contract import ConciseContract
 from flask import Flask, jsonify, request
 import json
 
@@ -9,21 +8,33 @@ with open('../blockchain/build/contracts/Manager.json') as f:
     info_json = json.load(f)
 
 w3 = Web3(HTTPProvider("http://localhost:8540"))
+w3.eth.defaultAccount = w3.eth.accounts[0]
 
 manager = w3.eth.contract(
     address="0x4D73457De69bE9a0C8524660Ff67f261D2Ff2c7f",
     abi=info_json['abi']
 )
-w3.eth.defaultAccount = w3.eth.accounts[0]
 
 app = Flask(__name__)
+
+
+@app.route('/api/v1/balance', methods=['GET'])
+def get_balance():
+    balance = manager.functions.balance().call()
+    return jsonify({'balance': balance})
+
 
 @app.route('/api/v1/deposit', methods=['POST'])
 def deposit():
     content = request.json
-    tx_hash = manager.functions.deposit().transact({'from': w3.eth.accounts[0], 'value': content['amount']})
+    tx_hash = manager.functions.deposit().transact(
+        {'from': w3.eth.accounts[0], 'value': content['amount']}
+    )
     w3.eth.waitForTransactionReceipt(tx_hash)
     balance = manager.functions.balance().call()
-    return jsonify({'new_balance':balance})
+    return jsonify({'new_balance': balance})
 
-app.run()
+
+if __name__ == '__main__':
+    app.run()
+
